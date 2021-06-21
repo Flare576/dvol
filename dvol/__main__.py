e1b42c2407fbd41c733dd0133e971c5edb707d65
flare576 <flare576@gmail.com>
2021-06-21 09:35:47 -0500

    fix(setup): add setup framework

--- dvol/__main__.py
+++ dvol/__main__.py
@@ -4,7 +4,7 @@
 # TODO: when removing, should loop through existing mappings, delete those folders, then nuke the root/project folder
 # TODO: after removing files, recursivly delete empty folders upward
 
-version="0.0.5"
+version="0.0.6"
 import argparse
 
 from dvol_helpers.dvol_add import determine_add
